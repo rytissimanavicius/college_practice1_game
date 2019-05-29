b43cce294f3aff278922d38446cf1d15282710e0
Rytis Simanavičius <rytucis.qua@gmail.com>
2019-05-29 13:20:25 +0300

    zemelapioGeneravimoPradzia

--- praktikosDarbas/main.cpp
+++ praktikosDarbas/main.cpp
@@ -2,7 +2,41 @@
 
 using namespace std;
 
+const int zemPlotis = 40;
+const int zemAukstis = 10;
+char mas[zemAukstis][zemPlotis];
+
+struct zaidejas {
+
+};
+void generuotiZemelapi() {
+    for (int i = 0; i < zemAukstis; i++) {
+        for (int j = 0; j < zemPlotis; j++) {
+            mas[i][j] = '*';
+        }
+    }
+}
+void vaizduotiZemelapi() {
+    for (int i = 0; i < zemAukstis; i++) {
+        cout << "\n";
+        for (int j = 0; j < zemPlotis; j++) {
+            cout << mas[i][j];
+        }
+    }
+}
+void pozicijosZemelapiRadimas() {
+
+}
 int main() {
-    cout << "working?";
+    bool zemelapisSukurtas = false;
+    while (true) {
+        if (zemelapisSukurtas == false) {
+            generuotiZemelapi();
+            zemelapisSukurtas = true;
+        }
+        vaizduotiZemelapi();
+    }
+    int x, y;
+    //pozicijosZemelapiRadimas(x, y);
     return 0;
 }

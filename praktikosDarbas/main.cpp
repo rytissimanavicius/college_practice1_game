d9e32c6e34401e6c7063b0c939af5713238e87c3
Rytis Simanavičius <rytucis.qua@gmail.com>
2019-05-30 13:52:34 +0300

    Vaikščiojimas žemėlapyje veikia teisingai.

--- praktikosDarbas/main.cpp
+++ praktikosDarbas/main.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
-#include <random>
-#include <ctime>
 #include <fstream>
 #include <windows.h>
 #include <conio.h>
+#include <random>
+#include <ctime>
 
 using namespace std;
 
@@ -16,7 +16,7 @@ const int zemPlotis = 120;
 const int zemAukstis = 30;
 char zemelapis[zemAukstis][zemPlotis];
 
-struct zaidejas { //TODO: pildyt content
+struct zaidejas {                                    //TODO: pildyt content
 
 };
 void vaizduotiZemelapi() {
@@ -27,53 +27,67 @@ void vaizduotiZemelapi() {
         }
     }
 }
-void zemelapioAtsitiktinePozicija(int nuoKrasto, int &x, int &y) {
-    mt19937 gen;
-    gen.seed(time(0));
-    uniform_int_distribution<uint32_t> random1(nuoKrasto, zemPlotis - nuoKrasto);
-    uniform_int_distribution<uint32_t> random2(nuoKrasto, zemAukstis - nuoKrasto);
-    x = random1(gen);
-    y = random2(gen);
-}
-void zaidejoMatomumas(int &x, int &y) {
-    for (int i = y - 3; i < y + 4; i++) {
-        for (int j = x - 3; j < x + 4; j++) {
-            if (zemelapis[i][j] != 'Z' && zemelapis[i][j] != 'P') {
+void zaidejoMatomumas(int &x, int &y, int dungKord[]) {
+    int xDung, yDung;
+    for (unsigned int i = y - 3; i < y + 4; i++) {
+        for (unsigned int j = x - 3; j < x + 4; j++) {
+            for (int k = 0; k < 30; k+=2) {
+                if (zemelapis[i][j] != 'Z' && zemelapis[i][j] != 'D' && j == dungKord[k] && i == dungKord[k + 1]) zemelapis[i][j] = 'D';
+            }
+            if (zemelapis[i][j] != 'Z' && zemelapis[i][j] != 'M' && zemelapis[i][j] != '.' && zemelapis[i][j] != '^' && zemelapis[i][j] != 'D') {
                 zemelapis[i][j] = '.';
             }
         }
     }
 }
-void generuotiZemelapi(int &xZaid, int &yZaid, int &xPard, int &yPard, int xDung, int yDung) {
-    //zemelapis uzpildomas zvaigzdutemis
+void zemelapioAtsitiktinePozicija(int nuoKrasto, int &x, int &y) {                        //FIXME: kiti budai generuoja tuos pacius skaicius nors be time lib :/
+    mt19937 gen;
+    gen.seed(time(0));
+    uniform_int_distribution<uint32_t> random1(nuoKrasto, (zemPlotis - nuoKrasto) - 1);
+    uniform_int_distribution<uint32_t> random2(nuoKrasto, (zemAukstis - nuoKrasto) - 1);
+    x = random1(gen);
+    y = random2(gen);
+}
+void generuotiZemelapi(int &xZaid, int &yZaid, int &xPard, int &yPard, int xDung, int yDung, int dungKord[]) {
     for (int i = 0; i < zemAukstis; i++) {
         for (int j = 0; j < zemPlotis; j++) {
-            zemelapis[i][j] = '*';
+            if (i < 3 || i > zemAukstis - 4 || j < 3 || j > zemPlotis - 4) zemelapis[i][j] = '^';
+            else zemelapis[i][j] = '*';                                                             
         }
     }
-    //zaideja pastatome i atsitiktine pozicija, per tris vietas nuo zemelapio ribu
-    zemelapioAtsitiktinePozicija(4, xZaid, yZaid);
+
+
+
+
+
+    zemelapioAtsitiktinePozicija(3, xZaid, yZaid);
+    cout << xZaid << " " << yZaid << "/////";
     zemelapis[yZaid][xZaid] = 'Z';
     Sleep(1000);
-    //parduotuve pastatome i atsitiktine pozicija, kurioje nera zaidejo
+
+
+
+
+
     bool nesutampaKord = false;
     while (nesutampaKord == false) {
-        zemelapioAtsitiktinePozicija(1, xPard, yPard);
+        zemelapioAtsitiktinePozicija(3, xPard, yPard);
         if (xPard != xZaid && yPard != yZaid) nesutampaKord = true;
     }
-    zemelapis[yPard][xPard] = 'P';
+    zemelapis[yPard][xPard] = 'M';
     Sleep(1000);
-    //nustatome pozicija 5 pirmojo/antrojo/treciojo lygio dungeonu (jie bus atrasti tik tuo atveju, kai bus zaidejo matomumo zonoje)
-    ofstream dungeonKord("dungeonuKoordinates.txt");
-    if (dungeonKord.is_open()) {
-        for (int i = 0; i < 15; i++) {
-            zemelapioAtsitiktinePozicija(1, xDung, yDung);
-            dungeonKord << xDung << " " << yDung << " " << endl;
-            Sleep(1000);
-        }
+
+
+
+
+
+    for (int i = 0; i < 30; i+=2) {
+        zemelapioAtsitiktinePozicija(3, xDung, yDung);
+        dungKord[i] = xDung;
+        dungKord[i + 1] = yDung;  
+        cout << dungKord[i] << " " << dungKord[i + 1] << " / ";                                                                //TODO: patikrinti ar nesutampa su Z ir M
+        Sleep(1000);   
     }
-    else cout << "Nepavyko atidaryti failo!";
-    dungeonKord.close();
 }
 int main() {
     bool procVeikia = true;
@@ -91,10 +105,10 @@ int main() {
                 cout << "\nPASAULIS GENERUOJAMAS...\n";                          //TODO: krovima pademonstruoti
                 int xZaid, yZaid;
                 int xPard, yPard;
-                int xDung, yDung;
-                generuotiZemelapi(xZaid, yZaid, xPard, yPard, xDung, yDung);
+                int xDung, yDung, dungKord[30];
+                generuotiZemelapi(xZaid, yZaid, xPard, yPard, xDung, yDung, dungKord);
                 //nustatome zaidejo matomumo toli kuris yra 3, nezinoma zona zymima "*", o matoma "."
-                zaidejoMatomumas(xZaid, yZaid);
+                zaidejoMatomumas(xZaid, yZaid, dungKord);
                 while (true) {
                     vaizduotiZemelapi();
                     break;
@@ -122,43 +136,53 @@ int main() {
                                         break;
                                     }
                                     case KEY_UP: {
-                                        if (yZaid > 0) {
-                                            temp = zemelapis[yZaid - 1][xZaid];
+
+
+
+
+
+                                        if (yZaid > 3) {
+                                            temp = zemelapis[yZaid - 1][xZaid];                               //TODO: skaicius sutapatint su matmenimis
                                             zemelapis[yZaid - 1][xZaid] = zemelapis[yZaid][xZaid];            //TODO: uzejus pakeisti icona
                                             zemelapis[yZaid][xZaid] = temp1;                                  //TODO: prideti komentaru, pakeisti spalva i neutralesne
-                                            temp1 = temp;                                                     //TODO: zemelapio ribos kalnai
+                                            temp1 = temp;                                                     
                                             yZaid = yZaid - 1;                                               
-                                            if (yZaid > 2) zaidejoMatomumas(xZaid, yZaid);
+                                            if (yZaid > 2) zaidejoMatomumas(xZaid, yZaid, dungKord);
                                             vaizduotiZemelapi();
                                             cout << "\n";
                                             Sleep(50);
                                         }
                                         else cout << "PASIEKETE RIBA!\n";
                                         break;
+
+
+
+
+
                                     }
                                     case KEY_DOWN: {
-                                        if (yZaid < 29) {
+                                        if (yZaid < 26) {
                                             temp = zemelapis[yZaid + 1][xZaid];
                                             zemelapis[yZaid + 1][xZaid] = zemelapis[yZaid][xZaid]; 
                                             zemelapis[yZaid][xZaid] = temp1;
                                             temp1 = temp;
                                             yZaid = yZaid + 1;
-                                            if (yZaid < 27) zaidejoMatomumas(xZaid, yZaid);
+                                            if (yZaid < 27) zaidejoMatomumas(xZaid, yZaid, dungKord);              //FIXME: crash TIK i apacia is virsaus
                                             vaizduotiZemelapi();
-                                            cout << "\n";
+                                            cout << "\n";                                                  //FIXME: pakilus i virsu pilnai, griztant atgal crash
                                             Sleep(50);
                                         }
                                         else cout << "\nPASIEKETE RIBA!\n";
                                         break;
                                     }
                                     case KEY_LEFT: {
-                                        if (xZaid > 0) {
+                                        if (xZaid > 3) {
                                             temp = zemelapis[yZaid][xZaid - 1];
                                             zemelapis[yZaid][xZaid - 1] = zemelapis[yZaid][xZaid];
                                             zemelapis[yZaid][xZaid] = temp1;
                                             temp1 = temp;
                                             xZaid = xZaid - 1;
-                                            if (xZaid > 2) zaidejoMatomumas(xZaid, yZaid);
+                                            if (xZaid > 2) zaidejoMatomumas(xZaid, yZaid, dungKord);
                                             vaizduotiZemelapi();
                                             cout << "\n";
                                             Sleep(50);
@@ -167,13 +191,13 @@ int main() {
                                         break;
                                     }
                                     case KEY_RIGHT: {
-                                        if (xZaid < 119) {
+                                        if (xZaid < 116) {
                                             temp = zemelapis[yZaid][xZaid + 1];
                                             zemelapis[yZaid][xZaid + 1] = zemelapis[yZaid][xZaid]; 
                                             zemelapis[yZaid][xZaid] = temp1;
                                             temp1 = temp;
                                             xZaid = xZaid + 1;
-                                            if (xZaid < 117) zaidejoMatomumas(xZaid, yZaid);
+                                            if (xZaid < 116) zaidejoMatomumas(xZaid, yZaid, dungKord);
                                             vaizduotiZemelapi();
                                             cout << "\n";
                                             Sleep(50);
--- praktikosDarbas/tempCodeRunnerFile.cpp
+++ praktikosDarbas/tempCodeRunnerFile.cpp
@@ -1,22 +1,2 @@
- }
-                                    case KEY_UP: {
-                                        if (yZaid > 0) {
 
-
-
-                                            temp = zemelapis[yZaid - 1][xZaid];
-                                            zemelapis[yZaid - 1][xZaid] = zemelapis[yZaid][xZaid]; 
-                                            zemelapis[yZaid][xZaid] = temp1;
-                                            temp1 = temp;
-                                            
-
-
-                                            yZaid = yZaid - 1;
-                                            if (yZaid > 2) zaidejoMatomumas(xZaid, yZaid);
-                                            vaizduotiZemelapi();
-                                            cout << "\n";
-                                            Sleep(50);
-                                        }
-                                        else cout << "PASIEKETE RIBA!\n";
-                                        break;
-                                    }
+        cout << dungKord[i] << " " << dungKord[i + 1] << " / "; 
